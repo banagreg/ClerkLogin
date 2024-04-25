84418656bdfdf6247b1ded388e7812d73cf3373b
banagreg <bogdanovagergana97@gmail.com>
2024-04-25 17:48:55 +0300

    Added text to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold text-center mt-10">Home page</h1>
-    </div>
+    <>
+      <h1 className="text-2xl font-bold text-center mt-10">Welcome to ClerkLogin</h1>
+      <p className="mb-5 text-center">
+        This is the implementation of Clerk authentication.
+        <br />
+        Go ahead and sign in or sign up!
+      </p >
+    </>
   );
-}
+};
