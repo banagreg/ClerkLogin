5c29f89d012d2e11b18cf866825a6c7feb14a88c
banagreg <bogdanovagergana97@gmail.com>
2024-04-25 14:44:53 +0300

    Created Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+
+const Header = () => {
+	return (
+		<nav className='flex items-center justify-between px-6 py-4 mb-5 bg-black'>
+			<div className='flex items-center'>
+				<Link href='/'>
+					<div className='text-lg font-bold text-white uppercase'>
+						Clerk auth
+					</div>
+				</Link>
+			</div>
+
+			<div className='flex items-center text-white'>
+				<Link
+					href='sign-in'
+					className='text-gray-300 hover:text-white mr-4'
+				>
+					Sign In
+				</Link>
+				<Link
+					href='sign-up'
+					className='text-gray-300 hover:text-white mr-4'
+				>
+					Sign Up
+				</Link>
+			</div>
+		</nav>
+	)
+}
+
+export default Header
