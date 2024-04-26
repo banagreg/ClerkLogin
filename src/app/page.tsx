ce377b4646464dbd3e71e8b7014461e17008a231
banagreg <bogdanovagergana97@gmail.com>
2024-04-26 14:41:00 +0300

    Fixed position of paragraph in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl font-bold text-center mt-10">Welcome to ClerkLogin</h1>
-      <p className="mb-5 text-center">
+      <p className="mt-10 text-center">
         This is the implementation of Clerk authentication.
         <br />
-        Go ahead and sign in or sign up!
+        Go ahead and sign in/sign up so you can check our Dashboard!
       </p >
     </>
   );
